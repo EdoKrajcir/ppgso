b7ebbe7412c6fc59e777e2da602f806334287e84
Eduard Krajčír <edokrajcir@gmail.com>
2019-10-11 15:03:36 +0200

    po tretom cviku (task 2 done)

--- src/task1_filter/task1_filter.cpp
+++ src/task1_filter/task1_filter.cpp
@@ -10,6 +10,9 @@ const unsigned int SIZE = 512;
 // A simple RGB struct will represent a pixel in the framebuffer
 struct Pixel {
   // TODO: Define correct data type for r, g, b channel
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
 };
 
 int main()
@@ -19,19 +22,59 @@ int main()
 
   // TODO: Open file lena.raw (this is 512x512 RAW GRB format)
 
-  // TODO: Read data to framebuffer and close the file
+    FILE *file;
+    file = fopen("lena.raw","rb");
+    if (file == NULL) {printf("neny obrazok!!!\n");}
+
+
+    // TODO: Read data to framebuffer and close the file
+
+    fread( framebuffer, sizeof(Pixel), SIZE * SIZE, file );
+    fclose( file );
+
+
 
   // Traverse the framebuffer
   for (unsigned int y = 0; y < SIZE; y++) {
     for (unsigned int x = 0; x < SIZE; x++) {
       // TODO: Apply pixel operation
+
+
+      /*
+       * GREYSCALE
+      auto greyscale = static_cast<uint8_t >(0.3 * framebuffer[y][x].r + 0.59 * framebuffer[y][x].g + 0.11 * framebuffer[y][x].b);
+      framebuffer[y][x].r = greyscale;
+      framebuffer[y][x].g = greyscale;
+      framebuffer[y][x].b = greyscale;
+      */
+
+      /*
+       * INVERT
+      framebuffer[y][x].r = 255 - framebuffer[y][x].r;
+      framebuffer[y][x].g = 255 - framebuffer[y][x].g;
+      framebuffer[y][x].b = 255 - framebuffer[y][x].b;
+      */
+      framebuffer[y][x].r = static_cast<uint8_t>(framebuffer[y][x].r + 0.2 * (rand() % 255));
+      framebuffer[y][x].g = static_cast<uint8_t>(framebuffer[y][x].g + 0.2 * (rand() % 255));
+      framebuffer[y][x].b = static_cast<uint8_t>(framebuffer[y][x].b + 0.2 * (rand() % 255));
+
+      if (framebuffer[y][x].r > 255) {framebuffer[y][x].r = 255;}
+      if (framebuffer[y][x].g > 255) {framebuffer[y][x].g = 255;}
+      if (framebuffer[y][x].b > 255) {framebuffer[y][x].b = 255;}
     }
   }
 
   // TODO: Open file result.raw
   std::cout << "Generating result.raw file ..." << std::endl;
 
-  // TODO: Write the framebuffer to the file and close it
+    FILE *file2 = fopen("result.raw","wb");
+    if (file2 == NULL) {printf("neny obrazok!!!\n");}
+
+    // TODO: Write the framebuffer to the file and close it
+
+    fwrite( framebuffer, sizeof(Pixel), SIZE * SIZE, file2);
+    fclose(file2);
+
 
   std::cout << "Done." << std::endl;
   delete[] framebuffer;
--- src/task2_bresenham/task2_bresenham.cpp
+++ src/task2_bresenham/task2_bresenham.cpp
@@ -15,9 +15,93 @@ struct Point {
   int x,y;
 };
 
+struct Pixel {
+    // TODO: Define correct data type for r, g, b channel
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+};
+
 // Bresenham drawing algorithm
 void drawLine(ppgso::Image& framebuffer, Point& from, Point& to) {
   // TODO: Implement Bresenham drawing algorithm
+        int x = from.x;
+        int y = from.y;
+        //int m = 0;
+        //if ((to.x-from.x) == 0) {m = 99999999;}
+        //else {m = (to.y-from.y)/(to.x-from.x);}
+        int dx = abs(to.x-from.x), sx = from.x<to.x ? 1 : -1;
+        int dy = -abs(to.y-from.y), sy = from.y<to.y ? 1 : -1;
+        int e = dx+dy, e2;
+
+        while(true){
+            framebuffer.setPixel(x,y,255,255,255);
+            if (x==to.x && y==to.y) break;
+            e2 = 2*e;
+            if (e2 >= dy) {e += dy; x += sx;}
+            if (e2 <= dx) {e += dx; y += sy;}
+        }
+
+
+        /*if (m < -1) {
+            int e = -(to.x - from.x);
+            while (abs(x - to.x) > 0) {
+                framebuffer.setPixel(x, y, 255, 255, 255);
+                x += 1;
+                e += 2 * (to.y - from.y);
+                if (e > 0) {
+                    y += 1;
+                    e -= 2 * (to.x - from.x);
+                }
+            }
+        }
+
+         if ((m >= -1 ) && (m < 0)) {
+             int e = -(to.x - from.x);
+             while (abs(x - to.x) > 0) {
+                 framebuffer.setPixel(x, y, 255, 255, 255);
+                 x += 1;
+                 e += 2 * (to.y - from.y);
+                 if (e > 0) {
+                     y += 1;
+                     e -= 2 * (to.x - from.x);
+                 }
+             }
+
+             if ((m <= 1) && (m >= 0)) {
+                 int e = -(to.x - from.x);
+                 while (abs(x - to.x) > 0) {
+                     framebuffer.setPixel(x, y, 255, 255, 255);
+                     x += 1;
+                     e += 2 * (to.y - from.y);
+                     if (e > 0) {
+                         y += 1;
+                         e -= 2 * (to.x - from.x);
+                     }
+                 }
+             }
+             if (m > 1) {
+                 int e = -(to.y - from.y);
+                 while (abs(y - to.y) > 0) {
+                     framebuffer.setPixel(x, y, 255, 255, 255);
+                     y += 1;
+                     e += 2 * (to.x - from.x);
+                     if (e > 0) {
+                         x += 1;
+                         e -= 2 * (to.y - from.y);
+                     }
+                 }
+             }
+         }*/
+        /*while(x<=to.x) {
+            framebuffer.setPixel(x,y,255,255,255);
+            x += 1;
+            e += 2*(to.y - from.y);
+            if(e>0) {
+                y += 1;
+                e -= 2*(to.x-from.x);
+            }
+        }*/
 }
 
 int main()
@@ -27,6 +111,12 @@ int main()
 
   // TODO: Generate star points
   std::vector<Point> points;
+  points.insert(points.end(),{SIZE/2,0});
+  points.insert(points.end(),{0, SIZE/2});
+  points.insert(points.end(),{SIZE/2, SIZE});
+  points.insert(points.end(),{SIZE, SIZE/2});
+  points.insert(points.end(),{SIZE/2,0});
+  //points.insert(points.end(),{SIZE/2,SIZE});
 
   // Draw lines
   for(unsigned int i = 0; i < points.size() - 1; i++)
